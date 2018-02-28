708eb77ee1fd6a5401403ed1fcd5863c7a0b29b7
Jamie Sefton <jamie.sefton@parall.ax>
2018-02-28 18:19:47 +0000

    Updated to detect protocol. Handle auto handling. Added force protocol. Added in force redirect if set to be a certain protocol but not on that

--- config/protocol.php
+++ config/protocol.php
@@ -1,11 +1,29 @@
 <?php
 return [
+    /**
+     * Automatically detect based on current protocol
+     */
+    'auto' => false,
+
+    /**
+     * Force everything to a set protocol
+     * If blank then won't force anything
+     * Ignored it 'auto' is true.
+     */
+    'protocol' => '',
+
+    /**
+     * Use different rules per environment
+     * Ignored if 'auto' is true or 'protocol' is set.
+     */
     'environments' => [
         'local' => [
-            'https' => false
+            'https' => false,
+            'redirect' => false
         ],
         'production' => [
-            'https' => true
+            'https' => true,
+            'redirect' => false
         ]
     ]
 ];
--- src/ProtocolServiceProvider.php
+++ src/ProtocolServiceProvider.php
@@ -18,6 +18,7 @@ class ProtocolServiceProvider extends ServiceProvider
             __DIR__.'/../config/' => config_path(),
         ], 'protocol.config');
 
+       $this->handle();
     }
 
     /**
@@ -28,5 +29,87 @@ class ProtocolServiceProvider extends ServiceProvider
     public function register()
     {
 
+    }
+
+    public function handle()
+    {
+        $config = config('protocol');
+        $protocol = self::detectProtocol();
+        if ($config['auto'] == true) {
+            // Automatically handle based on current protocol
+            if ($protocol == 'https') {
+                \URL::forceScheme('https');
+            }
+        } elseif ($config['protocol'] !== '') {
+            // Force everything to set protocol
+            \URL::forceScheme($config['protocol']);
+            if ($protocol !== $config['protocol']) {
+                // redirect to set protocol;
+                self::redirectToProtocol($config['protocol']);
+            }
+        } else {
+            $this->handleEnvironments();
+        }
+    }
+
+    /**
+     * Redirect current location to correct protocol
+     * @param $protocol
+     */
+    public static function redirectToProtocol($protocol)
+    {
+        $redirect = $protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header('Location: ' . $redirect);
+    }
+
+    /**
+     * Check config for the current environment and
+     * force internal links to https if set
+     */
+    public function handleEnvironments()
+    {
+        $env = \App::environment();
+        if ($config = config('protocol.environments.' . $env)) {
+            $protocol = self::detectProtocol();
+            if ($config['https']) {
+                \URL::forceScheme('https');
+            }
+
+            if ($config['redirect'] && $config['https']) {
+                if ($protocol !== 'https') {
+                    self::redirectToProtocol('https');
+                }
+            }
+        }
+    }
+
+    /**
+     * Try our best to detect if its really https. Supports forward
+     * protocol if behind load balancing and CloudFront headers.
+     * Falls back to the most basic form of knowing by port.
+     * @return string
+     */
+    public static function detectProtocol()
+    {
+        $protocol = 'http';
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+            if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == "https") {
+                $protocol = 'https';
+            } else if (isset($_SERVER['HTTP_CLOUDFRONT_FORWARDED_PROTO'])) {
+                if ($_SERVER['HTTP_CLOUDFRONT_FORWARDED_PROTO'] == "https") {
+                    $protocol = 'https';
+                }
+            }
+        } else if (isset($_SERVER['HTTP_CLOUDFRONT_FORWARDED_PROTO'])) {
+            if ($_SERVER['HTTP_CLOUDFRONT_FORWARDED_PROTO'] == "https") {
+                $protocol = 'https';
+            }
+        } else if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+            $protocol = 'https';
+        } else if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443') {
+            $protocol = 'https';
+        }
+
+        return $protocol;
     }
 }
