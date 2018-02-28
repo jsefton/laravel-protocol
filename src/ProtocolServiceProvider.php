7d2edd0021d21b275d506028628b966cbc7b9fc7
Jamie Sefton <jamie.sefton@parall.ax>
2018-02-28 17:34:36 +0000

    Added initial files

---
+++ src/ProtocolServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JSefton\Protocol;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProtocolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
